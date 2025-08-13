406cfea768aafd7b515686e52ba10d91f7b61a62
CrispyDanger <crispyd2405@gmail.com>
2025-08-14 00:25:11 +0200

    create event app, add models for event, setup simple-jwt+endpoints

--- productivity-app/apps/event/models.py
+++ productivity-app/apps/event/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+Account = get_user_model()
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=20, blank=False, null=False)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(Account, related_name='user_events',
+                                   on_delete=models.CASCADE)
+
+
+class UserEvent(models.Model):
+    calendar = models.ForeignKey(Account, related_name='calendar_events',
+                                 on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
--- productivity-app/settings.py
+++ productivity-app/settings.py
@@ -154,3 +154,12 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
